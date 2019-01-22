51b5fe430a8b09cad5e502ebabdc5e228ca46cf2
Adam DeCosta <adam@decosta.io>
2019-01-22 14:07:17 -0500

    Commenting & generalizations of function definitions

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,37 @@ use rand::thread_rng;
 use std::cmp::Ordering;
 use std::io;
 
+// 'a designates the lifetime. Because these vectors are of reference strings
+// we must give a lifetime or they default to different lifetimes
+// without 'a it would default to Vec<&'a str>, Vec<&'b str>, thus not allowing
+// movement between the two collections as their lifetimes could end prematurely.
 fn draw_card<'a>(hand: &mut Vec<&'a str>, deck: &mut Vec<&'a str>) {
-    let card = deck.pop();
+    let card = deck.pop(); // returns an optional.
     match card {
-        Some(c) => hand.push(c),
+        Some(c) => hand.push(c), // If card is not 'None' then push to the hand
         None => println!("Deck is out of cards!"),
     }
 }
 
-fn calculate_score(hand: &Vec<&str>) -> i32 {
+// Calculates the score of the inputted hand.
+fn calculate_score(hand: &[&str]) -> i32 {
     let mut score = 0;
     let mut has_ace = false;
 
     for card in hand.iter() {
+        // adds the result of the match expression to score.
+        // if parsing card passes, just add the number.
+        // else it must be either an Ace or a Face card.
         score += match card.parse::<i32>() {
             Ok(num) => num,
             Err(_) => {
+                // hand.iter() goes through &str,
+                // so we must dereference card to compare str -> str.
                 if *card == "A" {
                     has_ace = true;
                     1
                 } else {
+                    // Must be a face card add 10
                     10
                 }
             }
@@ -38,7 +49,8 @@ fn calculate_score(hand: &Vec<&str>) -> i32 {
     score
 }
 
-fn print_status(player_cards: &Vec<&str>, dealer_cards: &Vec<&str>) {
+// Lists Player and Dealer cards and their respective scores.
+fn print_status(player_cards: &[&str], dealer_cards: &[&str]) {
     println!("Player's total is {:?}", calculate_score(player_cards));
     for card in player_cards.iter() {
         print!("{} ", card);
@@ -52,7 +64,8 @@ fn print_status(player_cards: &Vec<&str>, dealer_cards: &Vec<&str>) {
     println!();
 }
 
-fn shuffle(deck: &mut Vec<&str>) {
+// Shuffles the deck
+fn shuffle(deck: &mut [&str]) {
     let mut rng = thread_rng();
     deck.shuffle(&mut rng)
 }
@@ -76,11 +89,14 @@ fn main() {
     loop {
         println!("Do you want to (H)it, (S)tay, or (Q)uit? ");
 
+        // Gets the choice from user input
+        // Pressing enter adds '\n' to the end of the input.
         let mut choice = String::new();
         io::stdin()
             .read_line(&mut choice)
             .expect("Failed to read line");
 
+        // trim choice to get rid of the '\n' from the input.
         match choice.trim().to_uppercase().as_ref() {
             "H" => {
                 draw_card(&mut player_cards, &mut deck);
@@ -95,6 +111,7 @@ fn main() {
             _ => println!("Not a valid choice"),
         };
     }
+    // Dealer draws until score is above 17.
     while calculate_score(&dealer_cards) < 17 {
         draw_card(&mut dealer_cards, &mut deck);
     }
